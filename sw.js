5d4150ec3d380c9109adc960860ef64841735fb0
Bernat Masdeu <bernatmasdeutermens@gmail.com>
2026-05-01 19:45:40 +0200

    Bump cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'golf-stats-v8';
+const CACHE = 'golf-stats-v9';
 const FILES = ['./index.html', './data.js', './scorecards.js', './full_courses.js', './manifest.json'];
 
 self.addEventListener('install', e => {
