8f17cdfb47b984dbd6adccf6c770904c2f3ea916
Bernat Masdeu <bernatmasdeutermens@gmail.com>
2026-05-28 17:14:52 +0200

    Dades actualitzades maig 2026 - cache v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'golf-stats-v18';
+const CACHE = 'golf-stats-v19';
 const FILES = ['./index.html', './data.js', './scorecards.js', './full_courses.js', './gps_fontanals.js', './manifest.json'];
 
 self.addEventListener('install', e => {
